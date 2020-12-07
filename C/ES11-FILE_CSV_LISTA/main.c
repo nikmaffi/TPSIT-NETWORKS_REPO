6a7eff522f7e80ce4d063f093a04a699ee9a1ec1
Nicolò Maffi <71829227+nikmaffi@users.noreply.github.com>
2020-12-07 10:38:07 +0100

    Add files via upload

---
+++ C/ES11-FILE_CSV_LISTA/main.c
@@ -0,0 +1,47 @@
+/*
+ * Esercizio-5
+ * 
+ * File CSV (Comma Separated Value).
+ * Programma che, letto un file csv contenente dei brani musicali, propone a caso uno di essi.
+ *
+ * Autore:   Nicolò Maffi
+ * Data:     22/10/2020
+*/
+
+#include <stdio.h> //Per funzioni di input output
+#include <stdlib.h> //Per calloc(), srand() e rand()
+#include <time.h> //Per time()
+#include "song.h" //Per il tipo song
+
+#define NO_ERR 0 //Costante che indica una corretta esecuzione
+#define PARAM_ERR 1 //Costante che indica un errore sulla chiamata del programma
+#define FILE_ERR 2 //Costante che indica un errore di un operazione sul file
+
+int main(int argc, const char **argv, const char **envp)
+{
+    Playlist pl = initPlaylist();
+
+    srand(time(NULL)); //Il seme per il generatore di numeri casuali viene inizializzato
+
+    if(argc != 2) //La chiamata al programma deve avvenire con 2 paramatri
+    {
+        printf("\nErrore: Numero invalido di parametri.\n\n");
+
+        printf("SYNOPSIS\n");
+        printf("\t%s  file_csv\n\n", argv[0]);
+
+        return PARAM_ERR;
+    }
+
+    //Viene controllato se è possibile effettuare una lettura sul file
+    if(!reading(argv[1], &pl))
+    {        
+        printf("\nErrore: Non e' possibile effettuare la lettura del file %s.\n\n", argv[1]);
+
+        return FILE_ERR;
+    }
+
+    printsong(&pl, stdout); //Stampa sullo schermo delle canzoni in disordine
+
+    return NO_ERR;
+}
