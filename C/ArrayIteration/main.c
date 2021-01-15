620840b107f9920962589be8660250a5f2fb8cfe
Nicolò Maffi <71829227+nikmaffi@users.noreply.github.com>
2021-01-15 09:32:38 +0100

    Add files via upload

---
+++ C/ArrayIteration/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define DIM 10
+
+int main(int argc, const char **argv, const char **envp)
+{
+    int vet[DIM] = {-12, 45, -8, 78, -12, 89, 4, 6, 124, 0};
+
+    printf("\nFirst iteration\n");
+    for(int k = 0; k < DIM; k++)
+        printf("%d ", vet[k]);
+
+    printf("\n\nSecond iteration\n");
+    for(int k = 0; k < DIM; k++)
+        printf("%d ", *(vet + k));
+
+    printf("\n\nThird iteration\n");
+    for(int *pv = vet; pv < vet + DIM; pv++)
+        printf("%d ", *pv);
+
+    return 0;
+}
